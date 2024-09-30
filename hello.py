8d3c3f65623097c1f4dd65dabf52c8cf5342b857
Blue-Panther18 <olabisiabdusshakur01@gmail.com>
2024-09-30 15:13:45 +0100

    create a hello file

---
+++ hello.py
@@ -0,0 +1,6 @@
+"""
+This file mark the start of 100+ coding in python.
+This file simply takes in a user name then print Hello + username.
+"""
+username = input("Enter your name: ")
+print(f"Hello {username}, great to have you here!!!")
