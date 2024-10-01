26a4086ee7a92b4ef6d4917bcc267bd7a45b84b4
Blue-Panther18 <olabisiabdusshakur01@gmail.com>
2024-10-01 09:10:20 +0100

    created an arithmetic operation file

---
+++ simple_calculator.py
@@ -0,0 +1,11 @@
+"""
+This file performs simple arithmetic operations on two sets of integers.
+"""
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+print(f"\nAddition of {num1} and {num2} is {num1 + num2}.")
+print(f"Subtraction of {num2} from {num1} is {num1 - num2}.")
+print(f"Multiplication of {num1} and {num2} is {num1 * num2}.")
+print(f"Division of {num1} by {num2} is {num1 / num2}.\n")
