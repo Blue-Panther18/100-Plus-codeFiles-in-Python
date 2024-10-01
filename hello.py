011f986d1aeb80dd357767426b79052ba796c3a5
Blue-Panther18 <olabisiabdusshakur01@gmail.com>
2024-10-01 09:08:51 +0100

    modified the comment

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 """
-This file mark the start of 100+ coding in python.
+This file marks the start of 100+ coding in python.
 This file simply takes in a user name then print Hello + username.
 """
 username = input("Enter your name: ")
